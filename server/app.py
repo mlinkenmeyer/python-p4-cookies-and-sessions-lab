3c2d0510be6c359b8ddbd7b6cee3bee24734d947
Madison <mlinkenmeyer@gmail.com>
2023-10-13 11:25:13 -0700

    cookies and sessions lab

--- server/app.py
+++ server/app.py
@@ -15,20 +15,30 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+
 @app.route('/clear')
 def clear_session():
     session['page_views'] = 0
     return {'message': '200: Successfully cleared session data.'}, 200
+
 
 @app.route('/articles')
 def index_articles():
 
     pass
 
+
 @app.route('/articles/<int:id>')
 def show_article(id):
 
-    pass
+    session['page_views'] = session.get('page_views') or 0
+    session['page_views'] += 1
+
+    if session['page_views'] <= 3:
+        return Article.query.filter(Article.id == id).first().to_dict(), 200
+
+    return {'message': 'Maximum pageview limit reached'}, 401
+
 
 if __name__ == '__main__':
     app.run(port=5555)
